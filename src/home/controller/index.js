9711b69be85fe1afe37ded111f9f4895610bf955
kodo <524580860@qq.com>
2016-04-24 17:12:13 +0800

    add:主站初始化带入文章数据

--- src/home/controller/index.js
+++ src/home/controller/index.js
@@ -3,12 +3,15 @@
 import Base from './base.js';
 
 export default class extends Base {
-  /**
-   * index action
-   * @return {Promise} []
-   */
-  indexAction(){
-    //auto render template file index_index.html
-    return this.display();
-  }
+    /**
+     * index action
+     * @return {Promise} []
+     */
+    async indexAction() {
+        //auto render template file index_index.html
+        var article = await this.model('article').field({_id : 0}).select();
+        console.log(article)
+        this.assign({article : JSON.stringify(article)});
+        return this.display();
+    }
 }
